17a9726d4541dc57bd23c45f3a8fdcf0bf39bf14
YunaiV <zhijiantianya@gmail.com>
2023-09-14 23:08:25 +0800

    禁用 Quartz 组件
    启动时间从 18.5 降低到 18.0 秒，不稳定，预期在 0.5 吧
    
    比较大的好处，是简洁化了日志内容

--- yudao-framework/yudao-spring-boot-starter-job/src/main/java/cn/iocoder/yudao/framework/quartz/config/YudaoQuartzAutoConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-job/src/main/java/cn/iocoder/yudao/framework/quartz/config/YudaoQuartzAutoConfiguration.java
@@ -1,7 +1,6 @@
 package cn.iocoder.yudao.framework.quartz.config;
 
 import cn.iocoder.yudao.framework.quartz.core.scheduler.SchedulerManager;
-import org.quartz.Scheduler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,9 +12,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling // 开启 Spring 自带的定时任务
 public class YudaoQuartzAutoConfiguration {
 
+//    @Bean
+//    public SchedulerManager schedulerManager(Scheduler scheduler) {
+//        return new SchedulerManager(scheduler);
+//    }
+
     @Bean
-    public SchedulerManager schedulerManager(Scheduler scheduler) {
-        return new SchedulerManager(scheduler);
+    public SchedulerManager schedulerManager() {
+        return new SchedulerManager(null);
     }
 
 }
